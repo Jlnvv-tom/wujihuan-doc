cc063f281abcf2f8639002883b3e7584045a66a8
wujihuan <wujihuan@bingosoft.net>
2023-02-23 23:59:26 +0800

    : 文档项目

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+export default {
+  title: 'VitePress',
+  description: 'Just playing around.',
+  lang: 'en-US',
+  head: [
+    [
+      'link',
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }
+    ]
+    // would render: <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  ]
+}
